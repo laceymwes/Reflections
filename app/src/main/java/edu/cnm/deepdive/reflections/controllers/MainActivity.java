06250b955ca83d42b59e6a0d5e42d1bd11573d2c
Michael Lacey <laceymwes@gmail.com>
2018-06-10 07:48:50 -0600

    Initial commit Add Main activity

---
+++ app/src/main/java/edu/cnm/deepdive/reflections/controllers/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.reflections.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.reflections.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
